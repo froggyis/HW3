e3193e05100da367ac0763dbe4fb5ea7069a579f
froggyis <yuan65536@gapp.nthu.edu.tw>
2022-11-20 16:01:45 +0800

    placement

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
 
 
@@ -52,6 +46,11 @@ struct TreeNode
     void update();
 };
 
+vector<int> NPE ;//0~n stands for HB , -1 stands for V cut while -2 stands for H cut
+void InitializeNPE(vector<int> &NPE);
+void Placement(vector<int> &NPE);
+
+double floor_plan_region;
 int main(int argc, char *argv[])
 {
   //TODO 1. Parser
@@ -60,6 +59,7 @@ int main(int argc, char *argv[])
   in_hardblock.open(string(argv[1]));
   string line;
   string hb_name, tmp, tmp2, x1, y1, x2, y2, x3, y3, x4, y4;
+  pair<int, int> coor;
   int width, height;
   int x[4], y[4];
   while(getline(in_hardblock, line))
@@ -83,10 +83,13 @@ int main(int argc, char *argv[])
       y[3] = stoi(y4.substr(0, y4.size() - 1));
       width = *max_element(x, x+4) - *min_element(x, x+4);
       height = *max_element(y, y+4) - *min_element(y, y+4);
+
+      coor = make_pair( *min_element(x, x+4), *min_element(y, y+4) );
+
       // cout<<x1_<<" "<<y1_<<" "<<x2_<<" "<<y2_<<" "<<x3_<<" "<<y3_<<" "<<endl;
       
     }
-    HardBlock *HB = new HardBlock(hb_name, width, height);
+    HardBlock *HB = new HardBlock(hb_name, width, height, false, coor);
     HBList.emplace_back(HB);
 
   }
@@ -136,10 +139,88 @@ int main(int argc, char *argv[])
 
   //TODO 2 , initialization
   //TODO 2-1, calculate constraint
-  ifstream dead_space_ratio , in;
-  
+  int total_block_area = 0;
+  for (auto hb : HBList)
+    total_block_area += hb->height * hb->width ; 
 
+  double dead_space_ratiod = stof(string(argv[4]));
+  floor_plan_region = sqrt(total_block_area * (1+dead_space_ratiod));
+  //TODO 2-2, initialize NPE
+  InitializeNPE(NPE);
+  // for(auto c : NPE)
+  // {
+  //   cout<<c<<" ";
+  // }
+  Placement(NPE);;
 
   return 0;
 }
+
+// 12V3V4H.....
+
+void InitializeNPE(vector<int> &NPE)
+{
+  int row_cnt = 1 ;
+  int cur_width = 0;
+  auto HB = HBList[0];
+  cur_width += HB->width;
+  NPE.emplace_back(0);
+  for(int i = 1 ; i<HBList.size(); i++)
+  {
+    auto HB = HBList[i];
+    cur_width += HB->width;
+    if(cur_width <= floor_plan_region)//if current row can still fit with HB
+    {
+      
+      NPE.emplace_back(i);
+      NPE.emplace_back(-1);
+
+    }
+
+    else//if not , We go to new row and continue the process
+    {
+      cur_width = HB->width;
+      row_cnt=2;
+    }
+
+    if(row_cnt==2)//whenever we make two row, we need to inset an H cut into NPE
+    {
+      NPE.emplace_back(-2);
+      row_cnt=1;
+    }
+  }
+  NPE.emplace_back(-2);
+}
+
+void Placement(vector<int> &NPE)
+{
+  unordered_map<string, pair<int, int>> tmp;
+  int cor_x = 0, cor_y = 0;
+  int height_max = INT_MIN;
+  pair<int, int>cur_coor = make_pair(0, 0);
+  cout<<HBList.size()<<endl;
+  cout<<NPE.size()<<endl;
+  for(int i = 0 ; i<NPE.size()-1 ; i++)
+  {
+    if(NPE[i]!=-1 && NPE[i]!= -2)
+    {
+      auto HB = HBList[NPE[i]];      
+      cur_coor.first += HB->width;
+      if(HB->height > height_max) height_max = HB->height;
+      tmp[HB->name] = cur_coor;
+    }
+    if(NPE[i]==-2)
+    {
+      cur_coor.first = 0;
+      cur_coor.second = height_max;
+      height_max = INT_MIN;
+    }
+
+  
+  }
+  for(auto name : tmp)
+    cout<<"name : "<<name.first<<" x : "<<name.second.first<<"y : "<<name.second.second<<endl;
+
+}
+
 
