a2e983b5f8b93bfeae6d42d7908695b34c6e68b7
froggyis <yuan65536@gapp.nthu.edu.tw>
2022-11-29 16:44:49 +0800

    New makefile and split file into object

--- src/main.cpp
+++ src/main.cpp
@@ -14,40 +14,7 @@
 using namespace std;
 
 
-// struct pin
-// {
-//   string id; 
-//   int x_cor, y_cor;
 
-//   pin(string id, int x_cor, int y_cor):id(id), x_cor(x_cor), y_cor(y_cor) {}
-// };
-
-// struct HardBlock
-// {
-//   string name;
-//   int width, height;
-//   int downleft_x, downleft_y; 
-//   bool rotated;
-//   pin* center_pin;
-  
-//   void updateInfo(int& new_width, int& new_height, int& new_x, int& new_y);
-
-
-//     HardBlock(string name, int width, int height, pin* center_pin, int downleft_x = 0, int downleft_y = 0):
-//       name(name), width(width), height(height), rotated(false), downleft_x(downleft_x), downleft_y(downleft_y), center_pin(center_pin) {}
-// };
-
-// struct net
-// {
-//   int degree;
-//   vector<pin*> pins;
-//   vector<HardBlock*> hardblocks;
-
-//   net(int degree = 0):degree(degree) {}
-
-//   int calHPWL();
-
-// };
 
 void HardBlock::updateInfo(int& new_width, int& new_height, int& new_x, int& new_y)
 {
@@ -539,99 +506,6 @@ class SA
 
 
 
-
-
-// class Parser
-// {
-//   public:
-//     void read_hardblock(string const &filename)
-//     {
-//       ifstream in_hardblock(filename);
-//       string s;
-//       int width, height;
-//       int x[4], y[4];
-//       while(getline(in_hardblock, s))
-//       {
-//         if(s == "" || s[0] != 's') continue;
-//         stringstream ss(s);
-//         string hb_name, tmp1, tmp2, x0, y0, x1, y1, x2, y2, x3, y3;
-
-//         while(ss >> hb_name >> tmp1 >> tmp2 >> x0 >> y0 >> x1 >> y1 >> x2 >> y2 >> x3 >> y3)
-//         {
-//             x[0] = stoi(x0.substr(1, x0.size() - 2));
-//             y[0] = stoi(y0.substr(0, y0.size() - 1));
-
-//             x[1] = stoi(x1.substr(1, x1.size() - 2));
-//             y[1] = stoi(y1.substr(0, y1.size() - 1));
-
-//             x[2] = stoi(x2.substr(1, x2.size() - 2));
-//             y[2] = stoi(y2.substr(0, y2.size() - 1));
-
-//             x[3] = stoi(x3.substr(1, x3.size() - 2));
-//             y[3] = stoi(y3.substr(0, y3.size() - 1));
-//             width = *max_element(x, x+4) - *min_element(x, x+4);
-//             height = *max_element(y, y+4) - *min_element(y, y+4);
-
-//             int center_x = *min_element(x, x+4) + width/2;
-//             int center_y = *min_element(y, y+4) + height/2;
-//             pin* center_pin = new pin(hb_name, center_x, center_y);
-//             HardBlock *HB = new HardBlock(hb_name, width, height, center_pin, x[0], y[0]);
-//             HBList.emplace_back(HB);
-//             HBTable[hb_name] = HB;
-//             PinTable[hb_name] = HB->center_pin;
-//         }
-//       }
-//     }
-
-
-//     void read_net(string const &filename)
-//     {
-//       ifstream fin_pl(filename);
-//       string pin_name;
-//       int x_cor, y_cor;
-//       while(fin_pl >> pin_name >> x_cor >> y_cor)
-//       {
-//       pin *cur_pin = new pin(pin_name, x_cor, y_cor);
-//       PinTable[pin_name] = cur_pin;
-//       }
-//     }
-
-    
-//     void read_pin(string const &filename)
-//     {
-
-//         ifstream fin_nets(filename);
-//         string tmp;
-//         while(getline(fin_nets, tmp))
-//         {
-//           if(tmp[3] != 'D') continue;
-//           stringstream ss(tmp);
-//           string temp, colon;
-//           int degree;
-//           while(ss >> temp >> colon >> degree)
-//           {
-//               net *cur_net = new net(degree);
-//               NetList.emplace_back(cur_net);
-//               for(int i = 0; i < degree; ++i)
-//               {
-//                 string terminal;
-//                 fin_nets >> terminal;
-//                 if(terminal[0] == 'p')
-//                 { 
-//                     auto fixed_pin = PinTable[terminal];
-//                     NetList.back()->pins.emplace_back(fixed_pin);
-//                 }
-//                 else
-//                 {
-//                     auto hb_pin = HBTable[terminal];
-//                     NetList.back()->hardblocks.emplace_back(hb_pin);
-//                 }
-//               }
-//           }
-//         }
-//     }
-
-// };
 
 
 int main(int argc, char *argv[])
